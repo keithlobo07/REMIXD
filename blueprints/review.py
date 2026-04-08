b882b147e77adad2bf3ee42542e2357d750290f4
finnmrose <finnmrose2@gmail.com>
2026-04-08 16:26:45 +0100

    changed review put func

--- blueprints/review.py
+++ blueprints/review.py
@@ -1,11 +1,11 @@
 from flask import *
 from app import sql
 from admin import is_admin
+from user import is_banned
 
 reviews = Blueprint('reviews', __name__)
 
-@reviews.route("/api/review/<userid>/<albumid>")
-def review_lookup(userid, albumid):
+def review_data(userid, albumid):
     cursor = sql.get_db().cursor()
 
     if 'id' in session:
@@ -13,7 +13,7 @@ def review_lookup(userid, albumid):
         data = cursor.fetchone()
         cursor.close()
 
-        return jsonify({
+        return {
             "accountID":data[0],
             "albumID":data[1],
             "timestamp":data[2],
@@ -23,13 +23,13 @@ def review_lookup(userid, albumid):
             "numLikes":data[6],
             "user_liked":data[7],
             "user_report":data[8]
-        })
+        }
     else:
         cursor.execute("SELECT AccountID, AlbumID, timestamp, Score, Liked, Content, (SELECT COUNT(*) FROM Tags WHERE Tags.ReviewAccountID = Review.AccountID AND Tags.ReviewAlbumID = Review.AlbumID AND Tags.info & 128) AS Likes FROM Review WHERE AccountID=%s AND AlbumID=%s;", (userid, albumid))
         data = cursor.fetchone()
         cursor.close()
 
-        return jsonify({
+        return {
             "accountID":data[0],
             "albumID":data[1],
             "timestamp":data[2],
@@ -37,51 +37,52 @@ def review_lookup(userid, albumid):
             "liked":data[4],
             "content":data[5],
             "numLikes":data[6]
-        })
+        }
+
+@reviews.get("/api/review/<userid>/<albumid>")
+def review_lookup(userid, albumid):
+    return jsonify(review_data(userid, albumid))
     
 @reviews.post("/api/review")
 def post_review():
     if not 'id' in session:
         return {"message": "not logged in"}, 409
     
-    cursor = sql.get_db().cursor()
-    cursor.execute("SELECT modTags FROM Account WHERE ID = %s;" %session['id'])
-    result = cursor.fetchone()[0]
-    if result & 1:
+    if is_banned():
         return {"message": "account banned from posting reviews"}, 409
 
     album_id, score, content = request.form['album_id'], request.form['score'], request.form['content']
 
     # finish when bethany finishes review page
 
-@reviews.put("/api/review")
-def update_review():
+@reviews.put("/api/review/<userid>/<albumid>")
+def update_review(account_id, album_id):
     if not 'id' in session:
         return {"message": "not logged in"}, 409
 
-    cursor = sql.get_db().cursor()
-    cursor.execute("SELECT modTags FROM Account WHERE ID = %s;" %session['id'])
-    result = cursor.fetchone()[0]
-    if result & 1:
-        return {"message": "account banned from posting reviews"}, 409
-    
-    album_id, score, content = request.form['album_id'], request.form['score'], request.form['content']
+    if is_banned():
+        return {"message": "account banned from editing reviews"}, 409
 
-    cursor.execute("SELECT * FROM Review WHERE AccountID = %s AND AlbumID = %s", (session['id'], album_id))
+    score, content = request.form['score'], request.form['content']
+
+    cursor = sql.get_db().cursor()
+
+    cursor.execute("SELECT * FROM Review WHERE AccountID = %s AND AlbumID = %s", (account_id, album_id))
     res = cursor.fetchone()
     if res == None:
-        return {"message": "no review found by user with id %s for album with id %s" %(session['id'], album_id)}, 404
-
-    cursor.execute("UPDATE Review SET Score=%s, Content=%s WHERE ID = %s;", (score, content, session['id']))
+        return {"message": "no review found by user with id %s for album with id %s" %(account_id, album_id)}, 404
+    
+    if session['id'] == res[0] or is_admin():
+        cursor.execute("UPDATE Review SET Score=%s, Content=%s WHERE ID = %s;", (score, content, session['id']))
+        sql.get_db().commit()
+        return jsonify(review_data(account_id, album_id)), 200
 
     # finish when bethany finishes review page
 
-@reviews.delete("/api/review")
-def delete_review():
+@reviews.delete("/api/review/<userid>/<albumid>")
+def delete_review(account_id, album_id):
     if not 'id' in session:
         return {"message": "not logged in"}, 409
-    
-    account_id, album_id = request.form['account_id'], request.form['album_id']
     
     cursor = sql.get_db().cursor()
     cursor.execute("SELECT * FROM Review WHERE AccountID = %s AND AlbumID = %s;", (account_id, album_id))
