2deeb01eb1ef13cd79f02bd100bc8bd4635bd10c
finnmrose <finnmrose2@gmail.com>
2026-04-09 19:44:43 +0100

    minor bug fixes

--- sql_scripts/database_setup.sql
+++ sql_scripts/database_setup.sql
@@ -10,7 +10,6 @@ CREATE TABLE `Account` (
     Email varchar(255) NOT NULL,
     Password varchar(255) NOT NULL,
     Bio varchar(127),
-    PFP varchar(255),
     modFlags bit(8) NOT NULL default 0,
     modComment varchar(127),
     PRIMARY KEY (ID)
@@ -19,11 +18,11 @@ CREATE TABLE `Account` (
 DROP TABLE IF EXISTS `Review`;
 CREATE TABLE `Review` (
 	AccountID int NOT NULL,
-    AlbumID int NOT NULL,
+    AlbumID varchar(40) NOT NULL,
     timestamp timestamp NOT NULL default CURRENT_TIMESTAMP,
     Score tinyint NOT NULL,
     Liked Boolean NOT NULL,
-    Content varchar(127),
+    Content varchar(255),
     FOREIGN KEY (AccountID) REFERENCES Account(ID),
     PRIMARY KEY (AccountID,AlbumID)
 );
@@ -32,7 +31,7 @@ DROP TABLE IF EXISTS `Tags`;
 CREATE TABLE `Tags` (
 	AccountID int NOT NULL,
     ReviewAccountID int NOT NULL,
-    ReviewAlbumID int NOT NULL,
+    ReviewAlbumID varchar(40) NOT NULL,
     info bit(8) NOT NULL,
     FOREIGN KEY (AccountID) REFERENCES Account(ID),
     FOREIGN KEY (ReviewAccountID, ReviewAlbumID) REFERENCES Review(AccountID, AlbumID),
