601b8a14c354eaf82f545ee452500778c19cef6d
Keith Lobo <keithlobo0707@gmail.com>
2026-04-10 18:43:41 +0100

    tracklist teehee

--- static/js/adminDashboard_script.js
+++ static/js/adminDashboard_script.js
@@ -1,87 +1,103 @@
 function loadAnalytics() {
-
-    fetch("/api/admin/statistics").then(response => {
-        return response.json()
-
-    }).then(data => {
-        let graph_data = data.data;
-        if (graph_data === undefined) {
-            throw Error("Graph data retrieval failed.");
-        }
-
-            const ctx = document.getElementById('analytics_chart');
-
-        const chartData = {
-            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-            datasets: [{
-                backgroundColor: "rgba(210, 252, 255, 0.2)",
-                borderColor: "rgba(210, 252, 255, 1)",
-                data: graph_data, // Placeholder data - replace with actual data from backend
-                borderWidth: 1,
-                borderRadius: 5
-            }]
-        }
-
-        Chart.defaults.style = "bold";
-        Chart.defaults.color = "#EFE9FF";
-        Chart.defaults.font.family = "DM Sans, sans-serif";
-        Chart.defaults.plugins.tooltip.titleFont.size = 16;
-        Chart.defaults.plugins.tooltip.backgroundColor = "#1d4376";
-
-        new Chart(ctx, {
-            type: "bar",
-            data: chartData,
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                        ticks: {
-                            color: "#EFE9FF",
-                            font: {family: "DM Sans, sans-serif", size: 14},
-                            stepSize: 10 // Adjust step size as needed from backend data range
-                        }
-                    }
-                },
-                plugins: {
-                    legend: { display: false},
-                    title: {
-                        display: true,
-                        text: "Reviews in the Past 12 Months",
-                        color: "#EFE9FF",
-                        font: {
-                            family: "DM Sans, sans-serif",
-                            size: 25}
-                    },
-                    tooltip: {
-                        titleFont: {
-                            family: "DM Sans, sans-serif",
-                            size: 14
-                        }
-                    }
-                },
-                responsive: true,
-                maintainAspectRatio: false
-            }
-        });
-
-
+  fetch("/api/admin/statistics")
+    .then((response) => {
+      return response.json();
     })
+    .then((data) => {
+      let graph_data = data.data;
+      if (graph_data === undefined) {
+        throw Error("Graph data retrieval failed.");
+      }
+
+      const ctx = document.getElementById("analytics_chart");
+
+      const chartData = {
+        labels: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+        datasets: [
+          {
+            backgroundColor: "rgba(210, 252, 255, 0.2)",
+            borderColor: "rgba(210, 252, 255, 1)",
+            data: graph_data,
+            borderWidth: 1,
+            borderRadius: 5,
+          },
+        ],
+      };
+
+      Chart.defaults.style = "bold";
+      Chart.defaults.color = "#EFE9FF";
+      Chart.defaults.font.family = "DM Sans, sans-serif";
+      Chart.defaults.plugins.tooltip.titleFont.size = 16;
+      Chart.defaults.plugins.tooltip.backgroundColor = "#1d4376";
+
+      new Chart(ctx, {
+        type: "bar",
+        data: chartData,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              ticks: {
+                color: "#EFE9FF",
+                font: { family: "DM Sans, sans-serif", size: 14 },
+                stepSize: 10, // Adjust step size as needed from backend data range
+              },
+            },
+          },
+          plugins: {
+            legend: { display: false },
+            title: {
+              display: true,
+              text: "Reviews in the Past 12 Months",
+              color: "#EFE9FF",
+              font: {
+                family: "DM Sans, sans-serif",
+                size: 25,
+              },
+            },
+            tooltip: {
+              titleFont: {
+                family: "DM Sans, sans-serif",
+                size: 14,
+              },
+            },
+          },
+          responsive: true,
+          maintainAspectRatio: false,
+        },
+      });
+    });
 }
 
-import {review_card} from "./components/review_card.js";
+import { review_card } from "./components/review_card.js";
 
 export function add_reviews(albumid) {
-    review_section = document.getElementById("review_section")
+  review_section = document.getElementById("review_section");
 
-    fetch(`/api/admin/reviews`).then(response => {
-        return response.json()}).then(data => {
-            let reviews = data.reviews;
-            reviews.forEach(review => {                
-                review_section.appendChild(review_card(review))
-            });
-        })
+  fetch(`/api/admin/reviews`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let reviews = data.reviews;
+      reviews.forEach((review) => {
+        review_section.appendChild(review_card(review));
+      });
+    });
 }
-
 
 /*
 function loadAnalytics0() {
--- static/js/albumView_script.js
+++ static/js/albumView_script.js
@@ -1,44 +1,50 @@
-import {review_card} from "./components/review_card.js";
+import { review_card } from "./components/review_card.js";
 
-export function add_reviews(albumid) {
-    review_section = document.getElementById("review_section")
+function add_reviews(albumid) {
+  review_section = document.getElementById("review_section");
 
-    fetch(`/api/album/${albumid}/reviews`).then(response => {
-        return response.json()}).then(data => {
-            let reviews = data.reviews;
-            reviews.forEach(review => {                
-                review_section.appendChild(review_card(review))
-            });
-        })
+  fetch(`/api/album/${albumid}/reviews`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let reviews = data.reviews;
+      reviews.forEach((review) => {
+        review_section.appendChild(review_card(review));
+      });
+    });
 }
 
+fetch("/api/album/11755c21-2546-4cb3-9b87-392f4f3c2fa2")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("HTTP error - currrent status ${response.status}");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    document.getElementById("albumTitle").innerText = data.albumName;
+    document.getElementById("albumCover").src = data.coverArt;
+    document.getElementById("albumRelease").innerText = data.releaseDate;
+    document.getElementById("albumArtist").innerText = data.artist;
+    //document.getElementById("albumRating").innerText = data.avgRating;
+    //document.getElementById("albumRatingNo").innerText = data.numReviews;
 
-fetch('/api/album/11755c21-2546-4cb3-9b87-392f4f3c2fa2')
-    .then(response => {
-        if (!response.ok){
-            throw new Error('HTTP error - currrent status ${response.status}');       
-        }
-        return response.json();
-    })
-    .then(data => {
-        document.getElementById("albumTitle").innerText = data.albumName;
-        document.getElementById("albumCover").src = data.coverArt;
-        document.getElementById("albumRelease").innerText = data.releaseDate;
-        document.getElementById("albumArtist").innerText = data.artist;
-        document.getElementById("albumRating").innerText = data.avgRating;
-        document.getElementById("albumRatingNo").innerText = data.numReviews;
+    //for the stars
+    const rating = parseFloat(data.avgRating);
+    const percent = (rating / 5) * 100;
 
-        //for the stars
-        const rating = parseFloat(data.avgRating);
-        const percent = (rating/5) * 100;
+    //document.getElementById("filledStars").style.width = "60%";
 
-        document.getElementById("filledStars").style.width = "60%";
-
-        //for the tracklist
-        const list = document.getElementById("tracklist");
-        list.innerHTML = TextTrackList.map(track => '<li>${track}</li>').join("");
-    })
-    .catch(error => {
-        console.error('Error fetching data', error);
-        alert('failed to gather data, please try again later')
+    //for the tracklist
+    const tracks = document.getElementById("tracklist");
+    data.trackList.forEach((track) => {
+      let trackElement = document.createElement("li");
+      trackElement.innerText = track.name;
+      tracks.appendChild(trackElement);
     });
+  })
+  .catch((error) => {
+    console.error("Error fetching data", error);
+    alert("failed to gather data, please try again later");
+  });
